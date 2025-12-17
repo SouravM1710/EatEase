c64351d03535d7ef079bca6ee5f1bd9f6ff86ef6
Soubhagya <soubhagya1007@gmail.com>
2025-12-17 16:45:11 +0530

    Added spring io and postgresql

---
+++ backend/src/main/java/com/eatease/backend/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.eatease.backend.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String greetings(){
+        return "Welcome to EatEase";
+    }
+}
